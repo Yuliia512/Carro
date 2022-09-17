59e2749438a9e23bf78f618067797fab495c9b8d
Yuliia512 <112338873+Yuliia512@users.noreply.github.com>
2022-09-18 00:10:06 +0300

    creation

---
+++ src/main/java/manager/HelperUser.java
@@ -0,0 +1,13 @@
+package manager;
+
+import org.openqa.selenium.WebDriver;
+
+public class HelperUser extends HelperBase{
+
+    public HelperUser(WebDriver wd) {
+        super(wd);
+    }
+
+
+
+}
